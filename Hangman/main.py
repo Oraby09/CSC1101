29b0a959c70b6238d1681c977365f7a43a7c923b
Oraby09 <101225929+Oraby09@users.noreply.github.com>
2022-03-20 21:15:33 -0500

    Add files via upload

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,77 +1,73 @@
-import re
+import re 
 
-# Get answer.__doc__
-answer_characters = "Dogs are cool"
-
+#Get answer. 
+answer_characters = "What's Up, Doc?" 
+ 
 answer_characters = answer_characters.upper()
 
-# Store known or unknown 
+# Store known or unknown letters. 
 answer_guesses = []
 
-
-for current_answer_character in answer_characters:
+# Determine which characters are known before starting the game. 
+for current_answer_character in answer_characters: 
   if re.search("^[A-Z]$", current_answer_character): 
-    answer_guesses.append(False)
-  else:
-    answer_guesses.append(True)
-
-#logic of playing the game
-guessed_letters = [] 
-num_of_incorrect_guesses = 0 
-
-while num_of_incorrect_guesses < 5 and False in answer_guesses:
-  print ("--------------------")
-  print("Guessed Letters: ", end = "")
-  
-  for current_guessed_letter in guessed_letters:
-    print(f"{current_guessed_letter} ", end = "")
-
-  print()
-
-  print(f"NUmber of inccorect guesses remaining: {5-num_of_incorrect_guesses} ")
-
-  print()
-
-  for answer_index in range(len(answer_characters)):
-    if answer_guesses[answer_index]:
-        print(answer_characters[answer_index], end = "")
-    else:
-        print ("_", end = "")
-        
-print()
-
-letter = input("Enter a letter: ")
-
-letter = letter.upper()
-
-
-if letter not in guessed_letters and len(letter) == 1 and re.search("^[A-Z]$", letter):
-   guessed_letters_insert_index = 0
-  
-   for current_guessed_letter in guessed_letters:
-     if letter < current_guessed_letter:
-       break
-
-     guessed_letters_insert_index += 1 
-
-    guessed_letters.insert (insert(index: int, object: _T) -> Noneguessed_letters_insert_index, letters)
- 
-  if letter in answer_characters:
-     #Letter is om the puzzle
-     for current_answer_index in range(len(answer_characters)):
-       if letter == answer_characters[current_answer_index]:
-        answer_guesses[current_answer_index] = True 
+    answer_guesses.append(False) 
   else: 
-      num_of_incorrect_guesses += 1  
-    
-  
-if num_of_incorrect_guesses < 5:
-  print ("Congrats you won!")
-else:
-  print("Sorry you ran out of guesses. L")
+    answer_guesses.append(True) 
+ 
+# Logic of playing the game.
+guessed_letters = [] 
+num_of_incorrect_guesses = 0
 
-print()
+while num_of_incorrect_guesses < 5 and False in answer_guesses: 
+  print("---------------------") 
+  print("Guessed Letters: ", end = "") 
+  
+  for current_guessed_letter in guessed_letters: 
+    print(f"{current_guessed_letter} ", end = "") 
+
+  print()
+
+  print(f"Number of incorrect guesses remaining: {5 - num_of_incorrect_guesses}")
+
+  print()
+
+  for answer_index in range (len(answer_characters)): 
+    if answer_guesses[answer_index]:
+      print(answer_characters[answer_index], end = "") 
+    else:
+      print("_", end = "")
+
+  print()
+
+  letter = input("Enter a letter: ")
+      
+  letter = letter.upper()
+
+  if letter not in guessed_letters and len(letter) == 1 and re.search("^[A-Z]$", letter):
+    guessed_letters_insert_index = 0
+    
+    for current_guessed_letter in guessed_letters: 
+      if letter < current_guessed_letter:
+        break
+
+      guessed_letters_insert_index += 1
+
+    guessed_letters.insert(guessed_letters_insert_index, letter)
+
+    if letter in answer_characters: 
+     # Letter is in the puzzle. 
+      for current_answer_index in range (len(answer_characters)): 
+        if letter == answer_characters[current_answer_index]:
+          answer_guesses[current_answer_index] = True
+    else:
+      num_of_incorrect_guesses += 1 
+
+# Game is over. Display whether user  won or not. 
+if num_of_incorrect_guesses < 5:
+  print("Congratulations, you solved the puzzle!") 
+else: 
+  print("Sorry, you ran out of guesses.") 
+
 
 print(f"{answer_characters} is the answer to the puzzle.")
-
-
